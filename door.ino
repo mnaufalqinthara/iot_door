93c3e0f754ec951a0e01d3a8f6fb4e376d4602f4
mnaufalqinthara <85345037+mnaufalqinthara@users.noreply.github.com>
2021-10-17 10:27:18 +0700

    pintu

---
+++ door.ino
@@ -0,0 +1,16 @@
+int relay_pin = 7;
+
+void setup() {
+  pinMode(relay_pin , OUTPUT);
+ 
+ 
+
+}
+
+void loop() {
+  digitalWrite(relay_pin , HIGH);
+  delay(5000);
+  digitalWrite(relay_pin , LOW);
+  delay(5000);
+  
+}
